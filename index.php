35507a556220d6492987264c90cb36010ecf48e8
Fernando Albino <fernandoalbino@gmail.com>
2019-06-24 20:03:32 -0300

    ajuste index e config

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php
+header("Content-Type: application/json");
+require_once("config.php");
 
-require_once "config.php";
-
+// require_once ("class/usuario.php");
 // $sql = new Sql();
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 // echo json_encode($usuarios);
 
-$root = new Usuario();
 
-$root->loadById(1);
+//Carrega um usuário
+// $root = new Usuario();
+// $root->loadbyId(2);
+// echo $root;
 
-echo $root;
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+//Carrega usuário usando login e a senha 
+$usuario = new Usuario();
+$usuario->login("fernando", "fa13sa11");
+
+echo $usuario;
 
 ?>
